43a8c115499e1144a6f34792d444864723372aac
dev.cappp <dev.cappp@gmail.com>
2025-02-24 19:30:47 +0900

    feat: logout 처리

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,6 +1,7 @@
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
 import Navigation from "./Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
@@ -9,7 +10,10 @@ const AppRouter = ({ isLoggedIn }) => {
       {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
-          <Route exact path="/" element={<Home />}></Route>
+          <>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/profile" element={<Profile />}></Route>
+          </>
         ) : (
           <Route exact path="/" element={<Auth />}></Route>
         )}
