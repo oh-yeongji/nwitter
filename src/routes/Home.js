93ac6ed6cb0413b66399e6f621ee57db282863ff
dev.cappp <dev.cappp@gmail.com>
2025-03-20 00:05:13 +0900

    feat: 사진 미리보기 구현

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,13 +5,14 @@ import Nweet from "components/Nweet";
 const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
   const [nweets, setNweets] = useState([]);
+  const [attachment, setAttachment] = useState("");
 
   useEffect(() => {
     dbService.collection("nweets").onSnapshot((snapshot) => {
       const newArray = snapshot.docs.map((document) => ({
         id: document.id,
         ...document.data(),
-      }));  
+      }));
       setNweets(newArray);
     });
   }, []);
@@ -35,6 +36,21 @@ const Home = ({ userObj }) => {
     setNweet(value);
   };
 
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onload = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    };
+    reader.readAsDataURL(theFile);
+  };
+
   return (
     <>
       <form onSubmit={onSubmit}>
@@ -45,14 +61,17 @@ const Home = ({ userObj }) => {
           placeholder="What's on your mind?"
           maxLength={120}
         />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Nweet" />
+        {attachment && <img src={attachment} width="50px" height="50px" />}
       </form>
       <div>
         {nweets.map((nweet) => (
-          <Nweet key={nweet.id}
-           nweetObj={nweet} 
-           isOwner={nweet.creatorId===userObj.uid}
-           />
+          <Nweet
+            key={nweet.id}
+            nweetObj={nweet}
+            isOwner={nweet.creatorId === userObj.uid}
+          />
         ))}
       </div>
     </>
